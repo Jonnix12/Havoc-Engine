172d03d5a57bffd3e8951a355bf3cb891faab0d2
Jonnix12 <jonharel48@gmail.com>
2022-12-15 21:36:32 +0200

    First

---
+++ Voyage_Engine/Voyage_Engine/Program.cs
@@ -0,0 +1,12 @@
+﻿using Voyage_Engine.Game_Engine.Engine;
+
+namespace Voyage_Engine
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainGameEngine mainGameEngine = new MainGameEngine();
+        }
+    }
+}
