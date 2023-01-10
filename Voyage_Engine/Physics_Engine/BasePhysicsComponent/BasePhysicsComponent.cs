1237ea0bf0bfd1010f28b06d79daf04c68dc0bf8
Liad Halotzi <liadhalotzi@gmail.com>
2023-01-10 14:11:32 +0200

    Adding bounds, collider and line before merge

---
+++ Voyage_Engine/Physics_Engine/BasePhysicsComponent/BasePhysicsComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voyage_Engine.Physics_Engine.BasePhysicsComponent
+{
+    internal class BasePhysicsComponent : Component
+    {
+        public BasePhysicsComponent() { }
+    }
+}
---
+++ Voyage_Engine/Physics_Engine/Collision/Line.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Voyage_Engine.Rendere_Engine.Vector;
+
+namespace Voyage_Engine.Physics_Engine.Collision
+{
+    internal class Line
+    {
+        private string _lineName;
+        private Vector2 _pointA;
+        private Vector2 _pointB;
+        private float _distance;
+
+        public Line(string lineName, Vector2 pointA, Vector2 pointB) 
+        { 
+            _lineName = lineName;
+            _pointA = pointA;
+            _pointB = pointB;
+            _distance = Vector2.Distance(_pointA, _pointB);
+        }
+    }
+}
